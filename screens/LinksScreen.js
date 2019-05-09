9c2a85ce103312c589a2661069fa8fa916a836b1
osmanmrtacar <osmanmrtacar@yandex.com>
2019-05-10 01:07:05 +0300

    new picker

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,37 +1,135 @@
 import React from 'react';
-import { Picker, StyleSheet, View, Text } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Animated, Button, Dimensions, Picker, Platform, StyleSheet, TouchableWithoutFeedback, Text, View } from 'react-native';
 
-const aArray = [
+const { width: WindowWidth } = Dimensions.get('window');
 
-]
-
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Appointments',
+export default class App extends React.Component {
+  state = {
+    language: 'js',
+    modalIsVisible: false,
+    modalAnimatedValue: new Animated.Value(0),
+    appointment: {
+      hospital: 'Select a hospital',
+      clinic: '',
+      doctor: ''
+    }
   };
-  state = {user: ''}
-   updateUser = (user) => {
-      this.setState({ user: user })
-   }
+
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      alert(
+        'Umm yeah this is meant for iOS, Picker modal looks different on Android. But go for it and try if you want, maybe make your own Snack that gives Android specific styling, that would be neat.'
+      );
+    }
+  }
+
+  _handlePressDone = () => {
+    Animated.timing(this.state.modalAnimatedValue, {
+      toValue: 0,
+      duration: 150,
+      useNativeDriver: true,
+    }).start(() => {
+      this.setState({ modalIsVisible: false });
+    });
+  };
+
+  _handlePressOpen = () => {
+    if (this.state.modalIsVisible) {
+      return;
+    }
+
+    this.setState({ modalIsVisible: true }, () => {
+      Animated.timing(this.state.modalAnimatedValue, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: true,
+      }).start();
+    });
+  };
+
   render() {
     return (
-      <View>
-            <Picker selectedValue = {this.state.user} onValueChange = {this.updateUser}>
-               <Picker.Item label = "DokuzEylul" value = "DEU" />
-               <Picker.Item label = "EGE" value = "EU" />
-               <Picker.Item label = "Ozel" value = "ozel" />
-            </Picker>
-            <Text style = {styles.text}>{this.state.user}</Text>
-         </View>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text>
+          </Text>
+          <Button title={this.state.appointment.hospital} onPress={this._handlePressOpen} />
+          <Button title="Select a clinic" onPress={this._handlePressOpen} />
+          <Button title="Select a doctor" onPress={this._handlePressOpen} />
+        </View>
+        {this._maybeRenderModal()}
+      </View>
     );
   }
+
+  _writeMe = (obj, index) => {
+    alert(index);
+    let appointment = { ...this.state.appointment };
+    appointment.hospital = obj;
+    this.setState({ appointment });
+  }
+
+  _maybeRenderModal = () => {
+    if (!this.state.modalIsVisible) {
+      return null;
+    }
+
+    const { modalAnimatedValue } = this.state;
+    const opacity = modalAnimatedValue;
+    const translateY = modalAnimatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [300, 0],
+    });
+
+    return (
+      <View
+        style={StyleSheet.absoluteFill}
+        pointerEvents={this.state.modalIsVisible ? 'auto' : 'none'}>
+        <TouchableWithoutFeedback onPress={this._handlePressDone}>
+          <Animated.View style={[styles.overlay, { opacity }]} />
+        </TouchableWithoutFeedback>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            transform: [{ translateY }],
+          }}>
+          <View style={styles.toolbar}>
+            <View style={styles.toolbarRight}>
+              <Button title="Done" onPress={this._handlePressDone} />
+            </View>
+          </View>
+          <Picker
+            style={{ width: WindowWidth, backgroundColor: '#e1e1e1' }}
+            selectedValue={this.state.appointment.hospital}
+            onValueChange={this._writeMe}>
+            <Picker.Item label="Denizli" value="dnz" />
+            <Picker.Item label="Izmir" value="izm" />
+            <Picker.Item label="Manisa" value="mns" />
+          </Picker>
+        </Animated.View>
+      </View>
+    );
+  };
 }
 
 const styles = StyleSheet.create({
-  text: {
-     fontSize: 30,
-     alignSelf: 'center',
-     color: 'red'
-  }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.65)',
+  },
+  toolbar: {
+    backgroundColor: '#f1f1f1',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+  },
+  toolbarRight: {
+    alignSelf: 'flex-end',
+  },
+});
